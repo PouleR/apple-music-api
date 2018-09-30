fe4d45195118b234cf2357f6c90298f149922bd4
Paul Radt <paul@spinninrecords.nl>
2018-09-30 19:02:24 +0200

    Add token generator

---
+++ src/AppleMusicAPITokenGenerator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace PouleR\AppleMusicAPI;
+
+use Jose\Factory\JWKFactory;
+use Jose\Factory\JWSFactory;
+
+/**
+ * Class AppleMusicAPITokenGenerator
+ */
+class AppleMusicAPITokenGenerator
+{
+    const TOKEN_TYPE = 'JWT';
+    const TOKEN_ALG = 'ES256';
+
+    /**
+     * @param string $teamId
+     * @param string $keyId
+     * @param string $keyFile
+     * @param int    $expiration
+     *
+     * @return string
+     */
+    static public function generateToken($teamId, $keyId, $keyFile, $expiration = 30)
+    {
+        $payload = [
+            'iss' => $teamId,
+            'exp' => time() + ($expiration * 86400),
+            'ait' => time(),
+        ];
+
+        $headers = [
+            'alg' => self::TOKEN_ALG,
+            'type' => self::TOKEN_TYPE,
+            'kid' => $keyId
+        ];
+
+        $key = JWKFactory::createFromKeyFile($keyFile);
+
+        return JWSFactory::createJWSToCompactJSON($payload, $key, $headers);
+    }
+}
